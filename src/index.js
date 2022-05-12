44b3dfe3c19b94da038d92607769e655f8531ec4
tkacikdominik <22526723+tkacikdominik@users.noreply.github.com>
2022-05-12 03:44:32 +0200

    try hashRouter

--- src/index.js
+++ src/index.js
@@ -10,15 +10,13 @@ import Grammar from "./routes/Grammar/Grammar"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      {/* <Routes>
+    <HashRouter>
+      <Routes>
         <Route path="/" element={<App />} />
-        <Route path="grammar" element={<Grammar />} />
-        <Route path="visualisation" element={<App />} />
-      </Routes> */}
-      {/*  */}
-    </BrowserRouter>
+        {/* <Route path="grammar" element={<Grammar />} />
+        <Route path="visualisation" element={<App />} /> */}
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
